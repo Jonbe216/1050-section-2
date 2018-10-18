2a795a1e63ac11a4d8f4da34267fc5955531b8b7
S00087738 <S00087738@ESS-TLC-016>
2018-10-18 18:35:49 -0400

    commit

---
+++ Person.cs
@@ -0,0 +1,43 @@
+﻿namespace objects_n_classes
+{
+    class Person
+    {
+        public string FirstName;
+
+        public string LastName;
+
+        public int age;
+
+        public Person Spouse;
+
+        public static double Sumofallages;
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+
+        public void PrintNameAndAge ()
+        {
+            System.Console.WriteLine ("Hi my name is " + this.GetFullName() + "!");
+            System.Console.WriteLine("And I am " + this.age + ".");
+        }
+
+
+
+      
+
+       
+
+        
+        
+
+        
+    }
+}
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace objects_n_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();  //First user
+
+            p1.AskUserForNameAndAge();
+            p1.CreateSpouseIfMarried();
+
+            Person p2 = new Person();
+
+            p2.AskUserForNameAndAge();
+            p2.CreateSpouseIfMarried();
+
+            System.Console.WriteLine("Average Age:" + Person.AverageAge());
+            System.Console.ReadKey(); 
+
+
+           
+
+
+         
+
+
+
+        }
+    }
+}
